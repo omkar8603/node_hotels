24745f0da535857ad786de78aad59d73fa095ad5
omkar8603 <omkarmane8603@gmail.com>
2024-10-07 15:44:53 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,42 +1,52 @@
 
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 
 //Define the mongoDB Connection URL
-const mongoURl = 'mongodb://localhost:27017/hotels'   // Replace 'mydatabase' with your database name
+//  const mongoURl = process.env.MONGODB_URL_LOCAL  // Replace 'mydatabase' with your database name
+// const mongoURl = 'mongodb+srv://omkarmane8603:Mongo123@cluster0.yza61.mongodb.net/'
+   const mongoURl = process.env.MONGODB_URL;
+// mongodb+srv://omkarmane8603:<db_password>@cluster0.yza61.mongodb.net/
+
+const db = async()=>{
+    await mongoose.connect(mongoURl)
+    console.log("db connect");
+}
+
 
 //set up MongoDB connection
-mongoose.connect(mongoURl, {
-    // useNewUrlParser : true,
-    // useUnifiedTopology : true
-})
+// mongoose.connect(mongoURl, {
+//     useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   //ssl: false // Disables SSL
+// })
 
-// get the default connection
-// Mongoose maintaines a default connection object representing the MongoDB connection
-const db = mongoose.connection;
+// // get the default connection
+// // Mongoose maintaines a default connection object representing the MongoDB connection
+// const db = mongoose.connection;
 
-db.on('connected',() => {
-    console.log('Connected to MongoDB server');
-})
+// db.on('connected',() => {
+//     console.log('Connected to MongoDB server');
+// })
 
-db.on('error',(err) => {
-    console.log('MongoDb connection error:' , err);
-})
+// db.on('error',(err) => {
+//     console.log('MongoDb connection error:' , err);
+// })
 
-db.on('disconnected',() => {
-    console.log('MongoDB disconnected');
-})
+// db.on('disconnected',() => {
+//     console.log('MongoDB disconnected');
+// })
 
 
 // Handling the process termination or interrupt signals
-process.on('SIGINT', async () => {
-    await mongoose.connection.close();
-    console.log('MongoDB disconnected due to app termination');
-    process.exit(0);
-});
+// process.on('SIGINT', async () => {
+//     await mongoose.connection.close();
+//     console.log('MongoDB disconnected due to app termination');
+//     process.exit(0);
+// });
 
 
-// Export the database connection
+// // Export the database connection
 
 module.exports = db;
 
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const db = require('./db')
-
+const db = require('./db');
+require('dotenv').config();
+db();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
  
-
+const PORT = process.env.PORT || 3000;
 
 
 app.get('/', function (req, res) {
@@ -30,7 +31,8 @@ const menuRouter = require('./routes/menuRoutes');
 app.use('/menuitems',menuRouter);
 
 
-app.listen(3000 , () => {
+
+app.listen(PORT, () => {
     console.log("Listing on port 3000");
 })
 
